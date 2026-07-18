acd7f9af8319c6e97b6dc56650445016cc04824e
Cozmopolit <rolf.schoefnagel@gmail.com>
2026-07-19 00:28:14 +0200

    chore(gui): remove periodic feedback dialog and entry counter

--- gui/src/pages/gui/Chat.tsx
+++ gui/src/pages/gui/Chat.tsx
@@ -44,16 +44,13 @@ import { isJetBrains, isMetaEquivalentKeyPressed } from "../../util";
 import { ToolCallDiv } from "./ToolCallDiv";
 
 import { useStore } from "react-redux";
-import FeedbackDialog from "../../components/dialogs/FeedbackDialog";
 
 import { DeprecationBanner } from "../../components/DeprecationBanner";
 import { FatalErrorIndicator } from "../../components/config/FatalErrorNotice";
 import InlineErrorMessage from "../../components/mainInput/InlineErrorMessage";
 import { resolveEditorContent } from "../../components/mainInput/TipTapEditor/utils/resolveEditorContent";
-import { setDialogMessage, setShowDialog } from "../../redux/slices/uiSlice";
 import { RootState } from "../../redux/store";
 import { cancelStream } from "../../redux/thunks/cancelStream";
-import { getLocalStorage, setLocalStorage } from "../../util/localStorage";
 import { EmptyChatBody } from "./EmptyChatBody";
 import { ExploreDialogWatcher } from "./ExploreDialogWatcher";
 import { useAutoScroll } from "./useAutoScroll";
@@ -213,18 +210,6 @@ export function Chat() {
         if (editorToClearOnSend) {
           editorToClearOnSend.commands.clearContent();
         }
-      }
-
-      // Increment localstorage counter for popup
-      const currentCount = getLocalStorage("mainTextEntryCounter");
-      if (currentCount) {
-        setLocalStorage("mainTextEntryCounter", currentCount + 1);
-        if (currentCount === 300) {
-          dispatch(setDialogMessage(<FeedbackDialog />));
-          dispatch(setShowDialog(true));
-        }
-      } else {
-        setLocalStorage("mainTextEntryCounter", 1);
       }
     },
     [dispatch, ideMessenger, reduxStore],
